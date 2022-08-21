b86620e6f5a1bde2a104b39b8e3ff43367746fea
ludoc0de <champi.ludo@tutanota.com>
2022-08-20 23:51:51 -0300

    remove getLogin

--- controllers/login.js
+++ controllers/login.js
@@ -1,8 +1,2 @@
 //Login controllers to models
 const RecipesArticle = require('../models/recipesArticle')
-
-module.exports = {
-    getLogin: (req, res) => {
-        res.render('login.ejs')
-    }
-}
