bf1a1574c9b0f257b09184e721aee4b7f11fc494
ludoc0de <champi.ludo@tutanota.com>
2022-08-26 23:52:36 -0300

    delete post

--- routes/home.js
+++ routes/home.js
@@ -1,6 +1,5 @@
 //Routes home
 //Get request home
-//Post artile
 
 const express = require('express')
 const router = express.Router()
@@ -8,6 +7,5 @@ const homeController = require('../controllers/home')
 
 //Route to the controllers
 router.get('/', homeController.getIndex)
-router.post('/', homeController.createArticle)
 
 module.exports = router
