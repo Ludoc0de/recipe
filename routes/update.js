ac219378da41b7102514d7b1c793bcca2a374b31
ludoc0de <champi.ludo@tutanota.com>
2022-08-29 16:52:00 -0300

    secure our update route

--- routes/update.js
+++ routes/update.js
@@ -3,9 +3,10 @@
 const express = require('express')
 const router = express.Router()
 const updateController = require('../controllers/update')
+const { userAuth } = require('../middleware/auth')
 
 //Route to the controllers
-router.get('/:id', updateController.getUpdate)
+router.get('/:id', userAuth, updateController.getUpdate)
 router.post('/:id', updateController.updateArticle)
 router.get('/remove/:id',updateController.deleteArticle)
 // router.post('/:id', putController.postPut)
