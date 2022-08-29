be1cc7597c0460d45fc10e6e6a0e466ac6d93903
ludoc0de <champi.ludo@tutanota.com>
2022-08-29 16:04:08 -0300

    create auth controller, add connect and logout

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,13 @@
 const RecipesArticle = require('../models/recipesArticle')
 
 module.exports = {
-    getTest: 
-
+    editConnect: (req,res) => {
+        res.redirect('/edit')
+    },
+    logout: (req,res) => {
+        req.logout(function(err) {
+        if (err) { return next(err); }
+        res.redirect('/');
+        });
+    }
 }
