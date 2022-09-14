755a3341a5fe57918f74917aa3c29c42223f1f5f
ludoc0de <champi.ludo@tutanota.com>
2022-09-14 19:58:44 -0300

    add multer to upload img from disk

--- routes/edit.js
+++ routes/edit.js
@@ -1,12 +1,14 @@
 //Edit routes
 const express = require('express')
 const router = express.Router()
+//upload image
+const upload = require("../middleware/multer");
 const editController = require('../controllers/edit')
 //secure edit route
 const { userAuth } = require('../middleware/auth')
 
 //Route to the controllers
 router.get('/', userAuth, editController.getEdit)
-router.post('/', editController.createArticle)
+router.post('/', upload.single("file"), editController.createArticle)
 
 module.exports = router
