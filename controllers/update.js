0219559c92e258346e6536904f0a4a3cbfb9004d
ludoc0de <champi.ludo@tutanota.com>
2022-09-21 17:57:43 -0300

    update updateArticle, can delete and post img

--- controllers/update.js
+++ controllers/update.js
@@ -33,39 +33,18 @@ module.exports = {
     // },
 
     // Update recipe title, article, images
-    // updateArticle: async (req, res)=>{
-    //     const id = req.params.id;
-    //     try{
-    //         // Upload array images
-    //         let resultImage = []
-    //         let resultId = []
-    //         const files = req.files
-    //         for (let file of files){
-    //             //loop the results cause can have more than 1 images path
-    //             const results = await cloudinary.uploader.upload(file.path);
-    //             //push each one in the []
-    //             resultImage.push(results.secure_url)
-    //             resultId.push(results.public_id)
-    //         }
-    //         await RecipesArticle.findByIdAndUpdate(id,
-    //             {
-    //                 title: req.body.title,
-    //                 image: resultImage,
-    //                 cloudinaryId: resultId,
-    //                 article:req.body.article,
-    //             })
-    //         res.redirect('/edit')
-    //     }catch(error){
-    //         if(error) return res.status(500).send(error);
-    //         res.redirect('/edit');
-    //     }
-    // },
-
-    // work on put
     updateArticle: async (req, res)=>{
         const id = req.params.id;
         try{
-            // Upload array images
+            //find recipe by id
+            let recipe = await RecipesArticle.findById(id);
+            //delete images from cloudinary
+            const cloudIds = recipe.cloudinaryId
+            for (let cloudId of cloudIds){
+                //loop cause can have more than 1 images path
+                await cloudinary.uploader.destroy(cloudId);
+            }
+            //Upload array images
             let resultImage = []
             let resultId = []
             const files = req.files
@@ -79,8 +58,8 @@ module.exports = {
             await RecipesArticle.findByIdAndUpdate(id,
                 {
                     title: req.body.title,
-                    $set: {image: resultImage},
-                    $set: {cloudinaryId: resultId},
+                    image: resultImage,
+                    cloudinaryId: resultId,
                     article:req.body.article,
                 })
             res.redirect('/edit')
