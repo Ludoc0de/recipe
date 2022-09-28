5374885cbf072f4bf278495001cac6bd641973ce
ludoc0de <champi.ludo@tutanota.com>
2022-09-28 11:24:10 -0300

    delete unused file

--- routes/recipe.js
+++
@@ -1,9 +0,0 @@
-//Recipe routes
-const express = require('express')
-const router = express.Router()
-const recipeController = require('../controllers/recipe')
-
-//Route to the controllers
-router.get('/:id', recipeController.getRecipe)
-
-module.exports = router
