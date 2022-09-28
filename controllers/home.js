65210cb267f407308af82c375edb7d6c1e92db21
ludoc0de <champi.ludo@tutanota.com>
2022-09-28 10:07:19 -0300

    merge recipe controllers

--- controllers/home.js
+++ controllers/home.js
@@ -1,5 +1,6 @@
 // Home controllers to models
 const RecipesArticle = require('../models/recipesArticle')
+const UserComment = require('../models/comment')
 
 module.exports = {
     // Display all recipes
@@ -7,6 +8,19 @@ module.exports = {
         try{
             const recipes = await RecipesArticle.find({ status: "public"})
             res.render('index.ejs', {recipe: recipes})
+        } catch(error) {
+            if(error) return res.status(500).send(error)
+        }
+    },
+    //Display recipe/id
+    getRecipe: async (req, res) => {
+        const id = req.params.id;
+        try{
+            const recipes = await RecipesArticle.find();
+            //get comments from the recipe id
+            const comments = await UserComment.find({recipe: id});
+            res.render('recipe.ejs', {recipe: recipes, idRecipe: id, comment: comments})
+            // idRecipe = id from recipeArticle id 
         } catch(error) {
             if(error) return res.status(500).send(error)
         }
