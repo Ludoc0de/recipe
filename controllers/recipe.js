d64902b2933fdf71e5bbe986e326790a3a4809e3
ludoc0de <champi.ludo@tutanota.com>
2022-09-27 22:44:24 -0300

    require userComment model, find all the comment link to the recipe id

--- controllers/recipe.js
+++ controllers/recipe.js
@@ -1,17 +1,19 @@
 //Recipe controllers to models
 const RecipesArticle = require('../models/recipesArticle')
+const UserComment = require('../models/comment')
 
 module.exports = {
     //Display recipe/id
     getRecipe: async (req, res) => {
         const id = req.params.id;
         try{
-            const recipes = await RecipesArticle.find()
-            res.render('recipe.ejs', {recipe: recipes, idArticle: id})
+            const recipes = await RecipesArticle.find();
+            //get comments from the recipe id
+            const comments = await UserComment.find({recipes: id});
+            res.render('recipe.ejs', {recipe: recipes, idArticle: id, comment: comments})
             // recipe, idArticle data from mongodb
         } catch(error) {
             if(error) return res.status(500).send(error)
         }
-    } 
-
+    }
 }
