e06961c3883487b799ac4fc5e46b3a7a5b77e275
ludoc0de <champi.ludo@tutanota.com>
2022-09-27 23:59:17 -0300

    add the id recipe

--- controllers/comment.js
+++ controllers/comment.js
@@ -4,13 +4,14 @@ const UserComment = require('../models/comment')
 module.exports = {
     //Create comment
      comment: async (req, res)=>{
-        console.log("create comment")
         try{
             await UserComment.create(
                 {
                     comment:req.body.comment,
                     name: req.body.name,
                     email: req.body.email,
+                    //create the comment with recipe id where created
+                    recipe: req.params.id
                 })
             //Redirect on the same page
             res.redirect('/recipe/'+ req.params.id)
