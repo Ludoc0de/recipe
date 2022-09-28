0f4be1048c09c09041538f94711fb51ac15faf55
ludoc0de <champi.ludo@tutanota.com>
2022-09-28 11:13:51 -0300

    merge update controller

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -40,5 +40,73 @@ module.exports = {
             if(error) return res.status(500).send(error);
             res.redirect('/dashboard');
         }
+    },
+    // Display id recipe
+    getUpdate: async (req, res) => {
+        const id = req.params.id;
+        try{
+            const recipes = await
+            await RecipesArticle.find()
+            res.render('update.ejs', {recipe: recipes, idArticle: id})
+        } catch(error) {
+            if(error) return res.status(500).send(error)
+        }
+    },
+     // Update recipe title, article, images
+    updateArticle: async (req, res)=>{
+        const id = req.params.id;
+        try{
+            //find recipe by id
+            let recipe = await RecipesArticle.findById(id);
+            //delete images from cloudinary
+            const cloudIds = recipe.cloudinaryId
+            for (let cloudId of cloudIds){
+                //loop cause can have more than 1 images path
+                await cloudinary.uploader.destroy(cloudId);
+            }
+            //Upload array images
+            let resultImage = []
+            let resultId = []
+            const files = req.files
+            for (let file of files){
+                //loop the results cause can have more than 1 images path
+                const results = await cloudinary.uploader.upload(file.path);
+                //push each one in the []
+                resultImage.push(results.secure_url)
+                resultId.push(results.public_id)
+            }
+            await RecipesArticle.findByIdAndUpdate(id,
+                {
+                    title: req.body.title,
+                    status: req.body.status,
+                    image: resultImage,
+                    cloudinaryId: resultId,
+                    article:req.body.article,
+                })
+            res.redirect('/dashboard')
+        }catch(error){
+            if(error) return res.status(500).send(error);
+            res.redirect('/dashboard');
+        }
+    },
+    //Delete recipe
+    deleteArticle: async (req, res) => {
+        const id = req.params.id;
+        try{
+            //find recipe by id
+            let recipe = await RecipesArticle.findById(id);
+            //delete images from cloudinary
+            const cloudIds = recipe.cloudinaryId
+            for (let cloudId of cloudIds){
+                //loop cause can have more than 1 images path
+                await cloudinary.uploader.destroy(cloudId);
+            }
+            //delete recipe article
+            await recipe.remove();
+            res.redirect("/dashboard");
+        }catch(error){
+            if(error) return res.status(500).send(error);
+            res.redirect('/dashboard');
+        }
     }
 }
