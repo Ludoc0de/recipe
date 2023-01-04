8d9e0e70d8a9a29013e473b490a9ef526e0f4a02
ludoc0de <champi.ludo@tutanota.com>
2023-01-04 17:58:36 -0300

    update

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,19 @@
-console.log("ok")
+const touch = document.querySelectorAll(".log");
+console.log(touch);
+touch.forEach((element) => {
+  element.addEventListener("click", toggle);
+});
 
-
-        
+function toggle() {
+  console.log("get it");
+  let flag = false;
+  let test = true;
+  console.log(flag);
+  touch.classList.toggle(test);
+  console.log(flag);
+  //   if (flag) {
+  //     console.log("ok true");
+  //   } else {
+  //     console.log("not true");
+  //   }
+}
