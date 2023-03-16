a0961a73c68785dcd54b8a9b1e450ed82d8f0606
ludoc0de <champi.ludo@tutanota.com>
2023-03-16 15:03:36 -0300

    update

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -1,130 +1,134 @@
 //get cloudinary middleware
 const cloudinary = require("../middleware/cloudinary");
 //Dashboard controllers to models
-const RecipesArticle = require('../models/recipesArticle')
-const UserComment = require('../models/comment')
+const RecipesArticle = require("../models/recipesArticle");
+const UserComment = require("../models/comment");
 
 module.exports = {
-    //Display all recipes
-    getDashboard: async (req, res) => {
-        try{
-            const recipes = await RecipesArticle.find()
-            res.render('dashboard.ejs', {recipe: recipes})
-        } catch(error) {
-            if(error) return res.status(500).send(error)
-        }
-    },
-    //Create article 
-     createArticle: async (req, res)=>{
-        try{
-            // Upload array images
-            let resultImage = []
-            let resultId = []
-            const files = req.files
-            for (let file of files){
-                //loop the results cause can have more than 1 images path
-                const results = await cloudinary.uploader.upload(file.path);
-                //push each one in the []
-                resultImage.push(results.secure_url)
-                resultId.push(results.public_id)
-            }
-            await RecipesArticle.create(
-                {
-                    title: req.body.title,
-                    status: req.body.status,
-                    image: resultImage,
-                    cloudinaryId: resultId,
-                    article:req.body.article,
-                })
-            res.redirect('/dashboard')
-        }catch(error){
-            if(error) return res.status(500).send(error);
-            res.redirect('/dashboard');
-        }
-    },
-    // Display id recipe
-    getUpdate: async (req, res) => {
-        const id = req.params.id;
-        try{
-            const recipes = await RecipesArticle.find();
-            //get comments from the recipe id
-            const comments = await UserComment.find({recipe: id});
-            res.render('update.ejs', {recipe: recipes, idRecipe: id, comment: comments})
-        } catch(error) {
-            if(error) return res.status(500).send(error)
-        }
-    },
-     // Update recipe title, article, images
-    updateArticle: async (req, res)=>{
-        const id = req.params.id;
-        try{
-            //find recipe by id
-            let recipe = await RecipesArticle.findById(id);
-            //delete images from cloudinary
-            const cloudIds = recipe.cloudinaryId
-            for (let cloudId of cloudIds){
-                //loop cause can have more than 1 images path
-                await cloudinary.uploader.destroy(cloudId);
-            }
-            //Upload array images
-            let resultImage = []
-            let resultId = []
-            const files = req.files
-            for (let file of files){
-                //loop the results cause can have more than 1 images path
-                const results = await cloudinary.uploader.upload(file.path);
-                //push each one in the []
-                resultImage.push(results.secure_url)
-                resultId.push(results.public_id)
-            }
-            await RecipesArticle.findByIdAndUpdate(id,
-                {
-                    title: req.body.title,
-                    status: req.body.status,
-                    image: resultImage,
-                    cloudinaryId: resultId,
-                    article:req.body.article,
-                })
-            res.redirect('/dashboard')
-        }catch(error){
-            if(error) return res.status(500).send(error);
-            res.redirect('/dashboard');
-        }
-    },
-    //Delete recipe
-    deleteArticle: async (req, res) => {
-        const id = req.params.id;
-        try{
-            //find recipe by id
-            let recipe = await RecipesArticle.findById(id);
-            //delete images from cloudinary
-            const cloudIds = recipe.cloudinaryId
-            for (let cloudId of cloudIds){
-                //loop cause can have more than 1 images path
-                await cloudinary.uploader.destroy(cloudId);
-            }
-            //delete recipe article
-            await recipe.remove();
-            res.redirect("/dashboard");
-        }catch(error){
-            if(error) return res.status(500).send(error);
-            res.redirect('/dashboard');
-        }
-    },
-    //comments
-     updateComments: async (req, res)=>{
-        const id = req.params.id;
-        try{
-            await UserComment.findByIdAndUpdate(id,
-                {
-                    comment:req.body.comment,
-                    name: req.body.name,
-                })
-            //Redirect on the same page
-            res.redirect('/dashboard')
-        }catch(error){
-            if(error) return res.status(500).send(error);
-            res.redirect('/dashboard');
-        }
+  //Display all recipes
+  getDashboard: async (req, res) => {
+    try {
+      const recipes = await RecipesArticle.find();
+      res.cookie("isAuthenticated", {
+        value: true,
+      });
+      res.render("dashboard.ejs", { recipe: recipes });
+    } catch (error) {
+      if (error) return res.status(500).send(error);
     }
-}
+  },
+  //Create article
+  createArticle: async (req, res) => {
+    try {
+      // Upload array images
+      let resultImage = [];
+      let resultId = [];
+      const files = req.files;
+      for (let file of files) {
+        //loop the results cause can have more than 1 images path
+        const results = await cloudinary.uploader.upload(file.path);
+        //push each one in the []
+        resultImage.push(results.secure_url);
+        resultId.push(results.public_id);
+      }
+      await RecipesArticle.create({
+        title: req.body.title,
+        status: req.body.status,
+        image: resultImage,
+        cloudinaryId: resultId,
+        article: req.body.article,
+      });
+      res.redirect("/dashboard");
+    } catch (error) {
+      if (error) return res.status(500).send(error);
+      res.redirect("/dashboard");
+    }
+  },
+  // Display id recipe
+  getUpdate: async (req, res) => {
+    const id = req.params.id;
+    try {
+      const recipes = await RecipesArticle.find();
+      //get comments from the recipe id
+      const comments = await UserComment.find({ recipe: id });
+      res.render("update.ejs", {
+        recipe: recipes,
+        idRecipe: id,
+        comment: comments,
+      });
+    } catch (error) {
+      if (error) return res.status(500).send(error);
+    }
+  },
+  // Update recipe title, article, images
+  updateArticle: async (req, res) => {
+    const id = req.params.id;
+    try {
+      //find recipe by id
+      let recipe = await RecipesArticle.findById(id);
+      //delete images from cloudinary
+      const cloudIds = recipe.cloudinaryId;
+      for (let cloudId of cloudIds) {
+        //loop cause can have more than 1 images path
+        await cloudinary.uploader.destroy(cloudId);
+      }
+      //Upload array images
+      let resultImage = [];
+      let resultId = [];
+      const files = req.files;
+      for (let file of files) {
+        //loop the results cause can have more than 1 images path
+        const results = await cloudinary.uploader.upload(file.path);
+        //push each one in the []
+        resultImage.push(results.secure_url);
+        resultId.push(results.public_id);
+      }
+      await RecipesArticle.findByIdAndUpdate(id, {
+        title: req.body.title,
+        status: req.body.status,
+        image: resultImage,
+        cloudinaryId: resultId,
+        article: req.body.article,
+      });
+      res.redirect("/dashboard");
+    } catch (error) {
+      if (error) return res.status(500).send(error);
+      res.redirect("/dashboard");
+    }
+  },
+  //Delete recipe
+  deleteArticle: async (req, res) => {
+    const id = req.params.id;
+    try {
+      //find recipe by id
+      let recipe = await RecipesArticle.findById(id);
+      //delete images from cloudinary
+      const cloudIds = recipe.cloudinaryId;
+      for (let cloudId of cloudIds) {
+        //loop cause can have more than 1 images path
+        await cloudinary.uploader.destroy(cloudId);
+      }
+      //delete recipe article
+      await recipe.remove();
+      res.redirect("/dashboard");
+    } catch (error) {
+      if (error) return res.status(500).send(error);
+      res.redirect("/dashboard");
+    }
+  },
+  //comments
+  updateComments: async (req, res) => {
+    const id = req.params.id;
+    try {
+      await UserComment.findByIdAndUpdate(id, {
+        comment: req.body.comment,
+        name: req.body.name,
+      });
+      //Redirect on the same page
+      res.redirect("/dashboard");
+    } catch (error) {
+      if (error) return res.status(500).send(error);
+      res.redirect("/dashboard");
+    }
+  },
+};
--- public/js/main.js
+++ public/js/main.js
@@ -4,12 +4,13 @@ touch.forEach((element) => {
   element.addEventListener("click", toggle);
 });
 
+const isAuthenticated = true;
 function toggle() {
   console.log("get it");
   let flag = false;
   let test = true;
   console.log(flag);
-  touch.classList.toggle(test);
+  touch.classList.toggle(flag);
   console.log(flag);
   //   if (flag) {
   //     console.log("ok true");
